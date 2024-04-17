f7fec42c3bad0282c4d29621669a7924b625eae7
AsanMem <amemetoff@gmail.com>
2024-04-17 18:25:48 +0400

    day page - done

--- src/app/day/page.tsx
+++ src/app/day/page.tsx
@@ -1,12 +1,25 @@
 import Header from "@/app/ui/header/Header";
+import { fetchPictureOfTheDay } from "../lib/data/fetchPictureOfTheDay";
+import HelloTittle from "../ui/hello-tittle";
 
-export default function Day() {
-    
+export default async function Day() {
+  const photoData = await fetchPictureOfTheDay()
 
-
-    return(
-      <main className="">
-      {/* <Header/> */}
-      <h1>Hello, day!</h1>
+  return (
+    <main className="">
+      <>
+        <HelloTittle mainText={photoData.title} supportiveText={`Today is ${photoData.date}, which means meet this beautiful snapshot of
+            the day`} />
+        <div className=" my-8 ">
+          <img
+            className="max-w-[75vw] h-auto mr-4 mb-1 rounded-sm object-contain max-h-[80vh] float-left shape-outside shape-square"
+            src={photoData.url}
+            alt={photoData.title}
+          />
+          <div className="text-gray-300 text-base md:text-xl lg:text-xl xl:text-2xl">
+            {photoData.explanation}
+          </div>
+        </div>
+      </>
     </main>)
-  }
+}
--- src/app/lib/utils.ts
+++ src/app/lib/utils.ts
@@ -30,3 +30,16 @@ export const generatePagination = (currentPage: number, totalPages: number) => {
       totalPages,
     ];
   };
+
+
+
+
+
+
+
+  export function formatDate(date = new Date()) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // добавляем ведущий ноль, если месяц состоит из одной цифры
+    const day = String(date.getDate()).padStart(2, '0'); // добавляем ведущий ноль, если день состоит из одной цифры
+    return `${year}-${month}-${day}`;
+  }
